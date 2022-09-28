facd7daf43c76cc24fe431dcbb3582d456565bbe
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2022-09-28 11:25:15 +0000

    added data files

---
+++ chapter05/src/components/BookData.js
@@ -0,0 +1,45 @@
+const books = [{
+    id: 1,
+    title: "天下統一への道",
+    author: "徳川家康",
+    price : 1200,
+    category_id : 1,
+},{
+    id: 2,
+    title: "効率的な刀狩の進め方",
+    author: "豊臣秀吉",
+    price : 800,
+    category_id : 3,
+},{
+    id: 3,
+    title: "恋愛中毒",
+    author: "清少納言",
+    price : 1400,
+    category_id : 2,
+},{
+    id: 4,
+    title: "誰でもできる幕府の作り方",
+    author: "源頼朝",
+    price : 1500,
+    category_id : 3,
+},{
+    id: 5,
+    title: "上手な幕府の終わらせ方",
+    author: "徳川慶喜",
+    price : 1200,
+    category_id : 3,
+},{
+    id: 6,
+    title: "参勤交代の傾向と対策",
+    author: "徳川家光",
+    price : 1800,
+    category_id : 1,
+},{
+    id: 7,
+    title: "鎌倉で愛を叫ぶ",
+    author: "北条政子",
+    price : 1400,
+    category_id : 2,
+}];
+
+export const getBookData = () => books;
---
+++ chapter05/src/components/CategoryData.js
@@ -0,0 +1,12 @@
+const categories = [{
+    id: 1,
+    name : '教養',
+},{
+    id: 2,
+    name : '小説',
+},{
+    id: 3,
+    name : 'ハウツー本',
+}];
+
+export const getCategoryData = () => categories;
